5b559ef621797dba99f988d4407355f7d7f1fe7e
asmloverX <asmlover.lee@gmail.com>
2014-04-20 16:00:49 +0800

    fixed Copyright of cpp-skills

--- SkillFramework/src/path_tools.cc
+++ SkillFramework/src/path_tools.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2012, ASMlove. All rights reserved.
+// Copyright (c) 2012, ASMlover. All rights reserved.
 // 
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
